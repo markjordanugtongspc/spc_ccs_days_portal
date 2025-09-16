7d836b5ad37b34ba8005221b41b0dc2a312a72b9
Mark Jordan Ugtong <159249841+markjordanugtongspc@users.noreply.github.com>
2025-09-16 10:52:55 +0800

    cursor-pointer

--- CCSDays/pages/events.php
+++ CCSDays/pages/events.php
@@ -348,7 +348,7 @@ if ($isPartial) {
             
             <!-- Action Buttons -->
             <div class="flex gap-4 mb-6">
-                <button id="createEventBtn" class="action-button bg-teal-light text-dark-1 hover:bg-teal-dark transition-all flex items-center">
+                <button id="createEventBtn" class="cursor-pointer action-button bg-teal-light text-dark-1 hover:bg-teal-dark transition-all flex items-center">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 mr-2">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                     </svg>
